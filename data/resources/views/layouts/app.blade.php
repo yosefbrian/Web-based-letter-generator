3e349ff0124b04a81ce7be882215e64c825867a8
yosefbrian <yosefbrian@gmail.com>
2016-11-05 08:11:35 +0700

    minor bug kurang kurawal

--- data/resources/views/layouts/app.blade.php
+++ data/resources/views/layouts/app.blade.php
@@ -196,7 +196,7 @@
 <!-- ./wrapper -->
 
 <!-- jQuery 2.2.3 -->
-<script src="plugins/jQuery/jquery-2.2.3.min.js"></script>
+<script src="{{ URL::asset('plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
 <!-- jQuery UI 1.11.4 -->
 <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
